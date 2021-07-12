ec119a3cec4b248cc558a4dda18298ef1a9ac4ca
r3surr3cti0n <matraattila2004@gmail.com>
2021-07-12 14:46:39 +0200

    make changes, hide box when the user clicks out of it

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const btn = document.querySelector(".share-btn");
+const btn = document.querySelector(".fa-share");
 const box = document.querySelector(".share-item-box");
-let isActive = false;
+let isActive;
 
-btn.addEventListener("click", () => {
-	if (!box.classList.contains("share-active")) {
-		isActive = false;
-	} else isActive = true;
+document.addEventListener("click", (e) => {
+	isActive = box.classList.contains("share-active");
 
-	if (isActive) {
+	// If active and we've not clicked on the
+	// box hide it
+	if (isActive && e.target !== box) {
 		box.classList.remove("share-active");
-	} else box.classList.add("share-active");
+	} else if (!isActive && e.target === btn) {
+		box.classList.add("share-active");
+	}
 });
